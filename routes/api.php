777c02bca61937ba21c3eea7fe729cbe4e30a453
AndhikaRCP <andhikarizky2603@gmail.com>
2025-06-17 20:57:56 +0700

    created all routes in web.php and api.php (not tested yet)

---
+++ routes/api.php
@@ -0,0 +1,38 @@
+<?php 
+
+use Illuminate\Support\Facades\Route;
+
+// Import semua controller
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\PelangganController;
+use App\Http\Controllers\SupplierController;
+use App\Http\Controllers\BarangController;
+use App\Http\Controllers\KategoriController;
+use App\Http\Controllers\PenjualanController;
+use App\Http\Controllers\DetailPenjualanController;
+use App\Http\Controllers\PembayaranPenjualanController;
+use App\Http\Controllers\PembelianController;
+use App\Http\Controllers\DetailPembelianController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes (Tanpa Middleware Auth)
+|--------------------------------------------------------------------------
+*/
+
+Route::get('/ping', function () {
+    return response()->json(['message' => 'API aktif']);
+});
+
+// CRUD API routes
+Route::apiResource('users', UserController::class);
+Route::apiResource('pelanggan', PelangganController::class);
+Route::apiResource('supplier', SupplierController::class);
+Route::apiResource('barang', BarangController::class);
+Route::apiResource('kategori', KategoriController::class);
+Route::apiResource('penjualan', PenjualanController::class);
+Route::apiResource('detail-penjualan', DetailPenjualanController::class);
+Route::apiResource('pembayaran-penjualan', PembayaranPenjualanController::class);
+Route::apiResource('pembelian', PembelianController::class);
+Route::apiResource('detail-pembelian', DetailPembelianController::class);
+
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,31 @@
 
 use Illuminate\Support\Facades\Route;
 
+// Controller import
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\PelangganController;
+use App\Http\Controllers\SupplierController;
+use App\Http\Controllers\BarangController;
+use App\Http\Controllers\KategoriController;
+use App\Http\Controllers\PenjualanController;
+use App\Http\Controllers\DetailPenjualanController;
+use App\Http\Controllers\PembayaranPenjualanController;
+use App\Http\Controllers\PembelianController;
+use App\Http\Controllers\DetailPembelianController;
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+// Route resource (CRUD) untuk semua tabel
+Route::resource('users', UserController::class);
+Route::resource('pelanggan', PelangganController::class);
+Route::resource('supplier', SupplierController::class);
+Route::resource('barang', BarangController::class);
+Route::resource('kategori', KategoriController::class);
+Route::resource('penjualan', PenjualanController::class);
+Route::resource('detail-penjualan', DetailPenjualanController::class);
+Route::resource('pembayaran-penjualan', PembayaranPenjualanController::class);
+Route::resource('pembelian', PembelianController::class);
+Route::resource('detail-pembelian', DetailPembelianController::class);
