d79986273c74dab67530788cbc82ff0d1331d0db
AndhikaRCP <andhikarizky2603@gmail.com>
2025-06-19 10:22:03 +0700

    update footer with company copyright

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -1,27 +1,13 @@
- <footer class="footer">
-          <div class="container-fluid d-flex justify-content-between">
-            <nav class="pull-left">
-              <ul class="nav">
-                <li class="nav-item">
-                  <a class="nav-link" href="http://www.themekita.com">
-                    ThemeKita
-                  </a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="#"> Help </a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="#"> Licenses </a>
-                </li>
-              </ul>
-            </nav>
-            <div class="copyright">
-              2024, made with <i class="fa fa-heart heart text-danger"></i> by
-              <a href="http://www.themekita.com">ThemeKita</a>
-            </div>
-            <div>
-              Distributed by
-              <a target="_blank" href="https://themewagon.com/">ThemeWagon</a>.
-            </div>
-          </div>
-        </footer>
+<footer class="footer">
+  <div class="container-fluid d-flex justify-content-between align-items-center flex-wrap py-3">
+
+    <div class="text-muted small">
+      © {{ now()->year }} PD Karya Citra Mandiri. All rights reserved.
+    </div>
+
+    <div class="text-muted small">
+      Sistem Manajemen Penjualan v1.0
+    </div>
+
+  </div>
+</footer>
